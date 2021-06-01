de59a182d2a7dbed6e6d0c50d49264c520e8acf8
Alex Vogt <vogtalex@oregonstate.edu>
2021-05-31 17:24:38 -0700

    Wrote third test, which tests whether Buzz is outputed when printFizz is given 5 as an input

--- testFizzBuzz.py
+++ testFizzBuzz.py
@@ -5,6 +5,7 @@ class testCaseAdd(unittest.TestCase):
 	def test_add(self):
 		self.assertEqual(fizzBuzz.printFizz(1), 1)
 		self.assertEqual(fizzBuzz.printFizz(3), "Fizz")
+		self.assertEqual(fizzBuzz.printFizz(5), "Buzz")
 
 
 
