7d68d999749e6c6572add773a369f6648cfd893f
Alex Vogt <vogtalex@oregonstate.edu>
2021-05-31 18:04:09 -0700

    Wrote third test, which tests when 400 is inputted to leapYearChecker that it returns Yes

--- testLeapYear.py
+++ testLeapYear.py
@@ -5,6 +5,7 @@ class testCaseAdd(unittest.TestCase):
 	def test_add(self):
 		self.assertEqual(leapYear.leapYearChecker(4), "Yes")
 		self.assertEqual(leapYear.leapYearChecker(100), "No")
+		self.assertEqual(leapYear.leapYearChecker(400), "Yes")
 
 
 
